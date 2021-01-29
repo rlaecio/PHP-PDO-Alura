0197f296ec9cc269ba100fcaa8e72258d191d745
Roque Laecio dos Santos <rlaecio@hotmail.com>
2021-01-29 02:42:28 +0000

    OO

--- inserir-aluno.php
+++ inserir-aluno.php
@@ -8,16 +8,16 @@ $pdo = new PDO('sqlite:' . $databasePath);
 
 $student = new Student(
     null, 
-    'Vinicius Dias', 
-    new \DateTimeImmutable('1997-10-15')
+    'Roque Souza', 
+    new \DateTimeImmutable('1977-07-13')
 );
 
-$sqlInsert = "INSERT INTO students (name, birth_date) VALUES (?, ?);";
+$sqlInsert = "INSERT INTO students (name, birth_date) VALUES (:name, :birth_date);";
 $statement  = $pdo->prepare($sqlInsert);
-$statement->bindValue(1, $student->name());
-$statement->bindValue(2, $student->birthDate()->format('Y-m-d'));
+$statement->bindValue(':name', $student->name());
+$statement->bindValue(':birth_date', $student->birthDate()->format('Y-m-d'));
 $statement->execute();
 
 if ($statement->execute()) {
-    echo 'Aluno inserido com sucesso!';
+    echo 'Aluno inserido com sucesso!' . PHP_EOL;
 }

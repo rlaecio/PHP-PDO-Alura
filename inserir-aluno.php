2010df7a6bde5a301bb4141527ef3a1e99db964e
Roque Laecio dos Santos <rlaecio@hotmail.com>
2021-01-29 02:20:56 +0000

    prepare statment

--- inserir-aluno.php
+++ inserir-aluno.php
@@ -6,8 +6,18 @@ require_once 'vendor/autoload.php';
 $databasePath = __DIR__ . '/banco.sqlite';
 $pdo = new PDO('sqlite:' . $databasePath);
 
-$student = new Student(null, 'Vinicius Dias', new \DateTimeImmutable('1997-10-15'));
-$sqlInsert = "INSERT INTO students (name, birth_date) VALUES ('{$student->name()}', '{$student->birthDate()->format('Y-m-d')}');";
+$student = new Student(
+    null, 
+    'Vinicius Dias', 
+    new \DateTimeImmutable('1997-10-15')
+);
 
+$sqlInsert = "INSERT INTO students (name, birth_date) VALUES (?, ?);";
+$statement  = $pdo->prepare($sqlInsert);
+$statement->bindValue(1, $student->name());
+$statement->bindValue(2, $student->birthDate()->format('Y-m-d'));
+$statement->execute();
 
-var_dump($pdo->exec($sqlInsert))
+if ($statement->execute()) {
+    echo 'Aluno inserido com sucesso!';
+}
